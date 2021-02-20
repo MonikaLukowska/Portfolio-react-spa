cb294a57f89f739d56486aadb6b07844ea53289e
Monia <monia@pcs-macbook-pro.home>
2021-02-20 20:13:56 +0100

    create flexbox component

---
+++ src/components/FlexBox/FlexBox.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+import { flexbox } from 'styled-system'
+
+const FlexBox = styled.div(
+  {
+    display:'flex',
+    flexWrap:'wrap',
+  },
+  flexbox
+)
+
+export default FlexBox
